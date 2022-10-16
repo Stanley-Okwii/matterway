75a7abc767ca4e1aea4c06847a9d89be1b00f615
StanleeO <stanlee.okwii@gmail.com>
2022-10-17 00:34:17 +0300

    (feat): Add error handling

--- src/crawler/amazon.ts
+++ src/crawler/amazon.ts
@@ -1,72 +1,61 @@
 import * as puppeteer from "puppeteer";
-import { Browser, Page, ElementHandle } from "puppeteer";
+import { Browser, Page } from "puppeteer";
 
 import { password, email, amazonLink, userAgent } from "../utils/constants";
-import {
-  waitForSelectorAndClick,
-  waitForSelectorAndType,
-  waitForElementAndClick,
-} from "../utils/helpers";
+import { waitForElementAndClick, loginToAmazon } from "../utils/helpers";
 
 export const addBookToAmazonCart = async (bookTitle: string) => {
   const browser: Browser = await puppeteer.launch({
     headless: false,
   });
   const page: Page = await browser.newPage();
-  await page.setUserAgent(userAgent);
-  await page.setViewport({ width: 1200, height: 1822 });
 
-  // Navigate book on amazon.com
-  await page.goto(amazonLink);
+  try {
+    await page.setUserAgent(userAgent);
+    await page.setViewport({ width: 1200, height: 1822 });
 
-  // Dismiss change country pop up modal
-  const dismissPopUp: ElementHandle<Element> | null = await page.$(
-    "input[data-action-type='DISMISS']"
-  );
-  if (dismissPopUp) {
-    await page.click("input[data-action-type='DISMISS']");
-  }
+    // Navigate book on amazon.com
+    await page.goto(amazonLink);
 
-  // Login into amazon if password and email exist
-  if (email && password) {
-    await waitForSelectorAndClick(
-      page,
-      "#nav-signin-tooltip > .nav-action-button > .nav-action-inner"
+    // Login into amazon if password and email exist
+    if (email && password) {
+      await loginToAmazon(page, email, password);
+    }
+
+    // Select book category to optimize search
+    await page.waitForSelector("#searchDropdownBox");
+    await page.click("#nav-search-dropdown-card");
+    await page.select(
+      "#searchDropdownBox",
+      "search-alias=stripbooks-intl-ship"
     );
-    await waitForSelectorAndType(page, "#ap_email", email);
-    await waitForSelectorAndClick(page, "#continue");
 
-    await waitForSelectorAndType(page, "#ap_password", password);
+    // Type random book title in the search input
+    await page.type("#twotabsearchtextbox", bookTitle);
+    await page.click("#nav-search-submit-button");
 
-    await waitForSelectorAndClick(page, "#signInSubmit");
+    // Select first result on the page
+    await page.waitForSelector("div[data-cel-widget='MAIN-SEARCH_RESULTS-1']");
+
+    // Select the first book available in Paperback format
+    const [PaperbackLink]: any = await page.$x(
+      '//a[contains(text(), "Paperback")]'
+    );
+    if (PaperbackLink) {
+      await PaperbackLink.click();
+    }
+
+    // Add book to cart
+    await waitForElementAndClick(page, "#add-to-cart-button");
+
+    // Navigate to the checkout page
+    await waitForElementAndClick(
+      page,
+      "input[data-feature-id='proceed-to-checkout-action']"
+    );
+  } catch (error) {
+    console.error("Error: ", error);
+    await page.close();
+    await browser.close();
   }
-
-  // Select book category to optimize search
-  await page.waitForSelector("#searchDropdownBox");
-  await page.click("#nav-search-dropdown-card");
-  await page.select("#searchDropdownBox", "search-alias=stripbooks-intl-ship");
-
-  // Type random book title in the search input
-  await page.type("#twotabsearchtextbox", bookTitle);
-  await page.click("#nav-search-submit-button");
-
-  // Select first result on the page
-  await page.waitForSelector("div[data-cel-widget='MAIN-SEARCH_RESULTS-1']");
-
-  // Select the first book available in Paperback format
-  const [PaperbackLink]: any = await page.$x(
-    '//a[contains(text(), "Paperback")]'
-  );
-  if (PaperbackLink) {
-    await PaperbackLink.click();
-  }
-
-  // Add book to cart
-  await waitForElementAndClick(page, "#add-to-cart-button");
-
-  // Navigate to the checkout page
-  await waitForElementAndClick(
-    page,
-    "input[data-feature-id='proceed-to-checkout-action']"
-  );
 };
--- src/crawler/goodReads.ts
+++ src/crawler/goodReads.ts
@@ -10,44 +10,51 @@ import { Genre } from "../utils/types";
 export const getRandomGoodReadsBookTitle = async () => {
   const browser: Browser = await puppeteer.launch();
   const page: Page = await browser.newPage();
+  let randomBookTitle: string = "";
 
-  // Navigate to goodreads page
-  await page.goto(goodReadsLink);
-  const choices: Array<Genre> | null = await page.$$eval(
-    "#categories > div > div.category",
-    (elements) => {
-      return elements.map((element: any) => element?.firstElementChild && ({
-        title: element.firstElementChild.innerText,
-        value: element.firstElementChild.href,
-      }));
-    }
-  );
+  try {
+    // Navigate to goodreads page
+    await page.goto(goodReadsLink);
+    const choices: Array<Genre> | null = await page.$$eval(
+      "#categories > div > div.category",
+      (elements) => {
+        return elements.map(
+          (element: any) =>
+            element?.firstElementChild && {
+              title: element.firstElementChild.innerText,
+              value: element.firstElementChild.href,
+            }
+        );
+      }
+    );
 
-  const selectedCategory: Answers<string> = await prompts([
-    {
-      type: "select",
-      name: "genre",
-      message: "Select preferred genre",
-      choices,
-    },
-  ]);
+    const selectedCategory: Answers<string> = await prompts([
+      {
+        type: "select",
+        name: "genre",
+        message: "Select preferred genre",
+        choices,
+      },
+    ]);
 
-  // Navigate to goodreads page containing books for selected category
-  await page.goto(selectedCategory.genre);
+    // Navigate to goodreads page containing books for selected category
+    await page.goto(selectedCategory.genre);
 
-  const bookTitles = await page.$$eval(
-    "div.poll > div.pollContents > div.pollAnswer > div.answerWrapper > div",
-    (elements) => {
-      return elements.map(
-        (element: Element) => element.getElementsByTagName("img")[0].alt
-      );
-    }
-  );
+    const bookTitles = await page.$$eval(
+      "div.poll > div.pollContents > div.pollAnswer > div.answerWrapper > div",
+      (elements) => {
+        return elements.map(
+          (element: Element) => element.getElementsByTagName("img")[0].alt
+        );
+      }
+    );
 
-  const randomBookTitle = bookTitles[getRandomNumber(0, 19)];
-
-  await page.close();
-  await browser.close();
-
+    randomBookTitle = bookTitles[getRandomNumber(0, 19)];
+  } catch (error) {
+    console.error("Error: ", error);
+  } finally {
+    await page.close();
+    await browser.close();
+  }
   return randomBookTitle;
 };
